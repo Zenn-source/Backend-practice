6943d7107ad3a4662ef4377ae8e2902dc55fed4a
Zenn-source <mackenzie.g.iguiron@gmail.com>
2025-10-05 19:35:53 +0800

    Initial commit

---
+++ Finance app/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+
+app.listen(port, (req, res) => {
+  console.log(`Server is online on http://localhost:${3000}`);
+});
