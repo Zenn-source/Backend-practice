04aa3765d69120144d5af1b8c07882089dc2caa7
Zenn-source <mackenzie.g.iguiron@gmail.com>
2025-10-06 21:41:49 +0800

    Test connection

--- Finance app/index.js
+++ Finance app/index.js
@@ -3,6 +3,10 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+  res.send(`<h1>Hello World!</h1>`);
+});
+
 
 app.listen(port, (req, res) => {
   console.log(`Server is online on http://localhost:${3000}`);
